b5c57669f698269a3adc224e41aeacc31cc7582b
flexkat <catherinemaritz1@gmail.com>
2019-07-14 17:37:58 +0100

    reach router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { render } from 'react-dom'
 import SearchParams from './SearchParams'
+import { Router, Link } from "@reach/router";
+import Details from './Details'
 
 const App = () => {
   // return React.createElement(
@@ -15,8 +17,13 @@ const App = () => {
 
   return (
     <div>
-      <h1>Adopt Me!</h1>
-      <SearchParams />
+      <header>
+        <Link to="/">Adopt Me!</Link>
+      </header>
+      <Router >
+        <SearchParams path="/" />
+        <Details path="/details/:id" />
+      </Router>
       {/* <Pet name="Luna" animal="Dog" breed="Havanese" />
       <Pet name="Pepper" animal="Bird" breed="Cockatiel" />
       <Pet name="Molly" animal="Dog" breed="Poodle" /> */}
--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import pet, { ANIMALS } from '@frontendmasters/pet'
+import Results from './Results'
 import useDropdown from './useDropdown';
 
 const SearchParams = () => {
@@ -8,6 +9,16 @@ const SearchParams = () => {
   const [breeds, setBreeds] = useState([]);
   const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS);
   const [breed, BreedDropdown, setBreed] = useDropdown("Breed", "", breeds);
+  const [pets, setPets] = useState([]);
+
+  async function requestPets() {
+    const { animals } = await pet.animals({
+      location,
+      breed,
+      type: animal
+    })
+    setPets(animals || []);
+  }
 
   useEffect(() => {
     setBreeds([]);
@@ -21,7 +32,10 @@ const SearchParams = () => {
 
   return (
     <div className="search-params">
-      <form action="">
+      <form onSubmit={e => {
+        e.preventDefault();
+        requestPets();
+      }}>
         <label htmlFor="location">
           Location
           <input id="location" value={location} placeholder="Location" onChange={e => setLocation(e.target.value)}/>
@@ -53,6 +67,7 @@ const SearchParams = () => {
           </label> */}
         <button>Submit</button>
       </form>
+      <Results pets={pets} />
     </div>
   );
 } 
